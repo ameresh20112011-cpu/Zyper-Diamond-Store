985c2346638551317dc0ed6196990f5984a3998b
Zyper <ameresh20112011@gmail.com>
2026-08-24 18:12:50 +0530

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,42 +1,71 @@
-```javascript
-// ==========================================
-// ZYPER DIAMOND STORE
-// ADMIN FIREBASE
-// ==========================================
+// ================================
+// ZYPER DIAMOND STORE - FIREBASE
+// ================================
+
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 import {
-    initializeApp,
-    getApps,
-    getApp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+    initializeAppCheck,
+    ReCaptchaV3Provider
+}
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-check.js";
 
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// ================================
+// FIREBASE CONFIG
+// ================================
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCd_2LplLOl4TOGJQWqndwHNGzPh2PEHM",
+
+    apiKey: "AIzaSyCd_2LplLOl4TOGJWQqndwHNGzPh2PEHM",
+
     authDomain: "zyper-d-iamond-store.firebaseapp.com",
+
     projectId: "zyper-d-iamond-store",
+
     storageBucket: "zyper-d-iamond-store.firebasestorage.app",
+
     messagingSenderId: "892467477866",
-    appId: "1:892467477866:web:61e9b6ff9435798da23913"
+
+    appId: "1:892467477866:web:61e9b6ff9435798da23913",
+
+    measurementId: "G-RLJFR6F2GC"
 };
 
-const app = getApps().length
-    ? getApp()
-    : initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
+// ================================
+// INITIALIZE FIREBASE
+// ================================
 
-export {
-    app,
-    auth,
-    db
-};
-```
+const app = initializeApp(firebaseConfig);
+
+
+// ================================
+// FIREBASE APP CHECK
+// ================================
+
+initializeAppCheck(app, {
+
+    provider: new ReCaptchaV3Provider(
+        "6LfBe2QtAAAAAJsp-crpLwMXkhPn1QhvRSVWzB8P"
+    ),
+
+    isTokenAutoRefreshEnabled: true
+
+});
+
+
+// ================================
+// EXPORT SERVICES
+// ================================
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
