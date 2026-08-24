833f0a00adbd2921ab099e0d399f862028e2d087
Zyper <ameresh20112011@gmail.com>
2026-08-24 14:14:02 +0530

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,8 +1,3 @@
-// ==========================================
-// ZYPER DIAMOND STORE
-// Firebase Configuration
-// ==========================================
-
 import { initializeApp }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
@@ -13,40 +8,25 @@ import { getFirestore }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
-// ==========================================
-// FIREBASE CONFIG
-// ==========================================
-
 const firebaseConfig = {
-
     apiKey: "AIzaSyCd_2LplLOl4TOGjbWQqndwHNGzPh2PEHM",
-
     authDomain: "zyper-d-iamond-store.firebaseapp.com",
-
     projectId: "zyper-d-iamond-store",
-
     storageBucket: "zyper-d-iamond-store.firebasestorage.app",
-
     messagingSenderId: "892467477866",
-
     appId: "1:892467477866:web:61e9b6ff9435798da23913",
-
     measurementId: "G-RLJFR6F2GC"
-
 };
 
 
-// ==========================================
-// INITIALIZE FIREBASE
-// ==========================================
-
 const app = initializeApp(firebaseConfig);
 
+const auth = getAuth(app);
 
-// ==========================================
-// FIREBASE SERVICES
-// ==========================================
+const db = getFirestore(app);
 
-export const auth = getAuth(app);
 
-export const db = getFirestore(app);
+export {
+    auth,
+    db
+};
