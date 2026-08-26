e0598a94549bd25a1fa0e6a20cde16b024467454
Zyper <ameresh20112011@gmail.com>
2026-08-26 15:47:29 +0530

    Update zyper-wallet-badge.js

--- zyper-wallet-badge.js
+++ zyper-wallet-badge.js
@@ -28,7 +28,7 @@ const WALLET_STYLE = {
 
         font: 11,
 
-        fontWeight: 800,
+        fontWeight: 1000,
 
         gap: 7,
 
@@ -49,7 +49,7 @@ const WALLET_STYLE = {
 
         font: 12,
 
-        fontWeight: 800,
+        fontWeight: 1000,
 
         gap: 7,
 
